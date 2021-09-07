64e25ec4e555d7ba68ca712999278b67162e7852
Tycho Hartingsveld <99062718@mydavinci.nl>
2021-09-07 13:11:25 +0200

    update

---
+++ person.py
@@ -0,0 +1,12 @@
+naam = input("Vul hier uw naam in: ")
+adres = input("Vul hier uw adres in: ")
+postcode = input("Vul hier uw postcode in: ")
+woonplaats = input("Vul hier uw woonplaats in: ")
+
+
+print(" ----------------------------------------------------")
+print("|  Naam      : " + naam + "                ")
+print("|  Adres     : " + adres + "           ")
+print("|  Postcode  : " + postcode + "                         ")
+print("|  Woonplaats: " + woonplaats + "                          ")
+print(" ----------------------------------------------------")
